67312840812a7dc93b884cc639a232e0bbc950b4
Fajar Jabbar <fajarjabbar093@gmail.com>
2026-04-21 14:08:49 +0500

    Added my first Python file

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("start this program") 
+print("/___|")
+print("   /|")
+print("  / |")
+print(" /  |")
+
+
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 def factorial(n):
     """Calculate the factorial of n."""
     if n < 0:
-<<<<<<< Updated upstream
-        raise ValueError("Factorial is not defined for negative numbers and will raise an error.")
-=======
         raise ValueError("Factorial is defined")
->>>>>>> Stashed changes
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
