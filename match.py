fa0d73e4d250bc49730dd89c0add5e7b3331e7da
Fajar Jabbar <fajarjabbar093@gmail.com>
2026-05-20 15:21:42 +0500

    Match statements completed

---
+++ match.py
@@ -0,0 +1,20 @@
+#-------------------------------------------MATCH STATEMENT---------------------------------
+# Value of the xpression is compared with the values of each case
+# associated block of code is executed
+day = 4
+match day:
+     case 1:
+          print("Monday")
+     case 2:
+          print("Tuesday")
+     case 3:
+          print("Wednesday")
+     case 4:
+          print("Thursday")
+     case 5:
+          print("Friday")
+     case 6:
+          print("Saturday")
+     case 7:
+          print("Sunday")
+          
