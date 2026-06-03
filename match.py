f0ae0bb177080c7f2923ef5804c953cec52bf74d
Fajar Jabbar <fajarjabbar093@gmail.com>
2026-06-03 10:52:08 +0500

    match loop

--- match.py
+++ match.py
@@ -17,4 +17,41 @@ match day:
           print("Saturday")
      case 7:
           print("Sunday")
+
+
+
+day = 5 
+match day:
+     case 6:
+          print("Today is Saturday")
+     case 7:
+          print("Today is Sunday")
+     case _:
+          print("Looking forward to the weekend")   # should be kept as the last case beacuse it always matches
+
+
+
+
+# ---- Combine values ----
+day = 4
+match day:
+     case 1 | 2 | 3 | 4 | 5:
+          print("Today is a weekday")
+     case 6 | 7:
+          print("I love Weekends!")
+
+
+
+# If Statement in the caes evaluation as an extra condition check
+
+month = 5
+day = 4
+match day:
+     case 1 | 2 | 3 | 4 | 5 if month == 4:
+          print("A weekday in April")
+     case 1 | 2 | 3 | 4 | 5 if month == 5:
+          print("A weekday in May")
+     case _:
+          print("No match")
+
           
