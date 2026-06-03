91f3c0804f1d3cb40131161d0b7c4f0c2cdf0fe8
Fajar Jabbar <fajarjabbar093@gmail.com>
2026-06-03 10:51:19 +0500

    if else

--- if_else.py
+++ if_else.py
@@ -333,3 +333,21 @@ if score > 90:
 print("score processed")
 
 
+# ---Pass with multiple conditions----
+value = -50
+if value < 0:
+    print("Negative value")
+elif value == 0:
+    pass
+else:
+    print("Positive value")
+
+
+
+
+def calculate_discpunt(price):
+    pass
+
+
+
+
